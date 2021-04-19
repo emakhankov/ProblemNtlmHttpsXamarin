8dc00c384fda508bdceb2eca61dc42af51d62026
Evgeny Makhankov <EMakhankov@gmail.com>
2021-04-19 09:49:32 +0300

    Start

---
+++ ProblemNTLM/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using NtlmHttpHandler;
+using Xamarin.Forms;
+
+namespace ProblemNTLM
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+
+        async void btnStart_Clicked(System.Object sender, System.EventArgs e)
+        {
+            
+
+            var handler = NtlmHttpHandlerFactory.Create();
+
+            handler.Credentials = new NetworkCredential(login.Text, password.Text, domain.Text); 
+
+            var httpClient = new HttpClient(handler);
+
+            var response = await httpClient.GetAsync(url.Text);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                result.Text = json;
+            }
+            
+        }
+    }
+}
